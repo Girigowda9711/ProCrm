5468423c857ebe97b4a3c43efe7782bd8b7e96f6
Girigowda9711 <girigowda9711@gmail.com>
2022-12-26 15:31:47 +0530

    first commit

---
+++ src/main/java/com/Procrm/TestBase/BaseClass.java
@@ -0,0 +1,50 @@
+package com.Procrm.TestBase;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+
+	WebDriver driver;
+	@BeforeMethod
+	public void setUp(String browserName) {
+
+		if(browserName.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver =new ChromeDriver();
+
+		}
+		else if(browserName.equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver=new FirefoxDriver();
+
+		}
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+		driver.manage().window().maximize();
+		driver.navigate().to("");
+
+	}
+
+
+
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+
+
+
+
+
+
+
+
+}
